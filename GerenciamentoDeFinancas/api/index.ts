a88dc1d0c36af987a977a388026890b313e73012
Samuel Ferreira <samueldbferreira@gmail.com>
2024-06-30 14:39:10 -0300

    api ip

--- GerenciamentoDeFinancas/api/index.ts
+++ GerenciamentoDeFinancas/api/index.ts
@@ -1,5 +1,7 @@
-const BASE_URL = "http://192.168.0.21:3000";
+const BASE_URL = "http://192.168.1.101:3000";
 
 export const SIGN_IN = `${BASE_URL}/auth`;
 
 export const USERS = `${BASE_URL}/users`;
+
+export const OBJECTIVES = `${BASE_URL}/objectives`;
