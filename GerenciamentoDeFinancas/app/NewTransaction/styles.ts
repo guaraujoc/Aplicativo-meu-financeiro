6f785615fe31869be763548be061e03535e44e89
Moniely Barboza <moniely.barboza@usp.br>
2024-06-30 14:17:10 -0300

    Separando estilo da main em um arquivo styles.ts

--- GerenciamentoDeFinancas/app/NewTransaction/styles.ts
+++ GerenciamentoDeFinancas/app/NewTransaction/styles.ts
@@ -64,4 +64,3 @@ export const styles = StyleSheet.create({
         fontWeight: "bold",
     },
 });
-
--- GerenciamentoDeFinancas/app/home/index.tsx
+++ GerenciamentoDeFinancas/app/home/index.tsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import {
 	View,
 	Text,
-	StyleSheet,
 	Image,
 	TouchableOpacity,
 	Alert,
 	ScrollView
 } from "react-native";
+import { styles } from "./styles";
 import { launchImageLibrary } from "react-native-image-picker";
 import Icon from "react-native-vector-icons/Ionicons";
 import MenuButton from "@/components/MenuButton";
@@ -143,104 +143,3 @@ export default function Index() {
 		</ScrollView>
 	);
 }
-
-const styles = StyleSheet.create({
-    scrollContainer: {
-        flex: 1,
-        backgroundColor: "#1D212A",
-    },
-    container: {
-        flexGrow: 1,
-    },
-	topBar: {
-		width: "100%",
-		flexDirection: "row",
-		justifyContent: "space-between",
-		alignItems: "center",
-		paddingTop: 48,
-		paddingBottom: 5,
-		paddingHorizontal: 10,
-		backgroundColor: "#12111E",
-	},
-	title: {
-		color: "#ffffff",
-		fontSize: 20,
-		fontWeight: "bold",
-	},
-	header: {
-		flexDirection: "row",
-		alignItems: "center",
-		marginBottom: 20,
-		width: "100%",
-		paddingTop: 20, // Adjust this value to include the top bar height and additional spacing
-	},
-	profilePicContainer: {
-		width: 150,
-		height: 150,
-		borderRadius: 360,
-		overflow: "hidden",
-		marginRight: 30,
-	},
-	profilePic: {
-		width: "100%",
-		height: "100%",
-	},
-	barsContainer: {
-		flex: 1,
-		flexDirection: "column",
-		justifyContent: "space-between",
-		height: 100,
-		marginRight: 20,
-	},
-	bar: {
-		height: 10,
-		borderRadius: 5,
-		backgroundColor: "#cccccc",
-	},
-	familySection: {
-		alignItems: "center",
-		marginBottom: 20,
-	},
-	familyTitle: {
-		color: "#ffffff",
-		fontSize: 18,
-		fontWeight: "bold",
-		marginBottom: 10,
-	},
-	familyPhotosContainer: {
-		flexDirection: "row",
-		justifyContent: "center",
-		gap: 10,
-	},
-	familyPhoto: {
-		width: 30,
-		height: 30,
-		borderRadius: 360,
-		marginHorizontal: 5,
-	},
-	objectiveTitle: {
-		color: "#ffffff",
-		fontSize: 10,
-		marginBottom: 3,
-	},
-	groupTitle: {
-		color: "#ffffff",
-		fontSize: 10,
-		marginTop: 100,
-		textDecorationLine: "underline",
-	},
-	gridContainer: {
-		flexDirection: "row",
-		flexWrap: "wrap",
-		justifyContent: "space-evenly",
-		margin: 5,
-		gap: 15,
-	},
-	text: {
-		color: "#ffffff", // Adiciona cor branca ao texto para melhor contraste com o fundo escuro
-	},
-	menuButton: {
-		width: 160, // Ajuste a largura conforme necessário
-		height: 100, // Ajuste a altura conforme necessário
-	},
-});
